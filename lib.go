e1f6eb3eb25648741bfe16806f24f8f21881435f
u-na-gi <orcinusorca1758dv6932@gmail.com>
2024-04-05 10:42:19 +0900

    fix argument type

--- lib.go
+++ lib.go
@@ -1,22 +1,32 @@
 package primitivedefaultvaluego
 
+import (
+	"errors"
+	"go/ast"
+)
+
 // defaultValue returns the default value for a given Go primitive type.
-func DefaultValue(fieldType string) string {
-	switch fieldType {
-	case "string":
-		return `""`
-	case "int", "int8", "int16", "int32", "int64",
-		"uint", "uint8", "uint16", "uint32", "uint64", "byte", "rune":
-		return "0"
-	case "float32", "float64":
-		return "0.0"
-	case "bool":
-		return "false"
-	case "complex64", "complex128":
-		return "0 + 0i"
-	default:
-		// Reference types (slices, maps, pointers, channels) and function types default to nil.
-		// For other types, such as structs or arrays, adjust as necessary.
-		return "nil"
+func DefaultValue(fieldType ast.Expr) (string, error) {
+	switch ft := fieldType.(type) {
+	case *ast.Ident:
+		switch ft.Name {
+		case "string":
+			return `""`, nil
+		case "int", "int8", "int16", "int32", "int64",
+			"uint", "uint8", "uint16", "uint32", "uint64", "byte", "rune":
+			return "0", nil
+		case "float32", "float64":
+			return "0.0", nil
+		case "bool":
+			return "false", nil
+		case "complex64", "complex128":
+			return "0 + 0i", nil
+		default:
+			// Reference types (slices, maps, pointers, channels) and function types default to nil.
+			// For other types, such as structs or arrays, adjust as necessary.
+			return "nil", nil
+		}
 	}
+	return "", errors.New("unknown type")
+
 }
